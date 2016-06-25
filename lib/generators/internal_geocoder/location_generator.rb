71a9c39ee407b14f6ecf134ae212a67a4a426d59
Henning Vogt <git@henvo.de>
2016-06-25 14:56:15 +0200

    Add migration number class method

--- lib/generators/internal_geocoder/location_generator.rb
+++ lib/generators/internal_geocoder/location_generator.rb
@@ -6,5 +6,13 @@ module InternalGeocoder
     def copy_migration_files
       migration_template 'migration/create_locations.rb', 'db/migrate/internal_geocoder_create_locations.rb'
     end
+
+    def self.next_migration_number(dirname)
+      if ActiveRecord::Base.timestamped_migrations
+        Time.new.utc.strftime("%Y%m%d%H%M%S")
+      else
+        raise NotImplementedError
+      end
+    end
   end
 end
