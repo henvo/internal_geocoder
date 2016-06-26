ddc2bc6d8f36f5fdcc584f92fb5414d2b12107c6
Henning Vogt <git@henvo.de>
2016-06-26 20:13:04 +0200

    Add new specs

--- spec/lib/internal_geocoder_spec.rb
+++ spec/lib/internal_geocoder_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe InternalGeocoder do
   let(:query) { '10178 Berlin' }
 
+  before { InternalGeocoder::Location.create!(
+    zip_code: '10178',
+    city: 'Berlin',
+    lat: 1, lon: 2
+  )}
+
   describe '.coordinates' do
     it 'should return a hash' do
       expect(InternalGeocoder.coordinates(query)).to be_instance_of Hash
@@ -10,6 +16,14 @@ describe InternalGeocoder do
 
     it 'should return a Hash with lat and lon' do
       expect(InternalGeocoder.coordinates(query)).to include(:lat, :lon)
+    end
+
+    it 'should get the correct lat' do
+      expect(InternalGeocoder.coordinates(query)[:lat]).to eql(1)
+    end
+
+    it 'should get the correct lon' do
+      expect(InternalGeocoder.coordinates(query)[:lon]).to eql(2)
     end
 
     context 'when full address is given' do
