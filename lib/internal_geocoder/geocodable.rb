d1f616dffa1460b4f1153a344feb35d14dcb0e78
Henning Vogt <git@henvo.de>
2016-06-26 22:21:23 +0200

    Add first shot of near scope

--- lib/internal_geocoder/geocodable.rb
+++ lib/internal_geocoder/geocodable.rb
@@ -3,7 +3,19 @@ module InternalGeocoder
     extend ActiveSupport::Concern
 
     included do
-      scope :near, -> { where(city: 'Berlin') }
+      scope :near, -> (lat, lon, radius) { find_by_sql(
+        "SELECT id, name, (
+           3959 * acos (
+           cos ( radians(78.3232) )
+           * cos( radians( #{lat} ) )
+           * cos( radians( #{lon} ) - radians(65.3234) )
+           + sin ( radians(78.3232) )
+           * sin( radians( #{lat} ) )
+           )
+         ) AS distance
+         HAVING distance < #{radius}
+         ORDER BY distance;"
+      )}
     end
   end
 end
