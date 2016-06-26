c215d6447741128324cb5ade8ab97e942dabb65b
Henning Vogt <git@henvo.de>
2016-06-26 22:44:08 +0200

    Fix radius query

--- lib/internal_geocoder/geocodable.rb
+++ lib/internal_geocoder/geocodable.rb
@@ -4,17 +4,18 @@ module InternalGeocoder
 
     included do
       scope :near, -> (lat, lon, radius) { find_by_sql(
-        "SELECT id, name, (
-           3959 * acos (
-           cos ( radians(78.3232) )
-           * cos( radians( #{lat} ) )
-           * cos( radians( #{lon} ) - radians(65.3234) )
-           + sin ( radians(78.3232) )
-           * sin( radians( #{lat} ) )
-           )
-         ) AS distance
-         HAVING distance < #{radius}
-         ORDER BY distance;"
+        "SELECT
+          id, (
+            3959 * acos (
+            cos ( radians(78.3232) )
+            * cos( radians( #{lat} ) )
+            * cos( radians( #{lon} ) - radians(65.3234) )
+            + sin ( radians(78.3232) )
+            * sin( radians( #{lat} ) )
+            )
+          ) AS distance
+        HAVING distance < #{radius}
+        ORDER BY distance;"
       )}
     end
   end
